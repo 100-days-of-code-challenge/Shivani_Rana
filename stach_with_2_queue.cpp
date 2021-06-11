cc74111b0996ec55d6a013f51e2ccdd619b1a3a8
Shivani08-spec <shivanirana03443@gmail.com>
2021-06-11 01:15:20 +0100

    day21

---
+++ stach_with_2_queue.cpp
@@ -0,0 +1,30 @@
+GFG question:- stack with 2 queue
+https://practice.geeksforgeeks.org/problems/stack-using-two-queues/1#
+
+
+int QueueStack :: pop()
+{
+        // Your Code  
+        if(q1.empty())
+        {
+            return -1;
+            
+        }
+        
+        
+            while(q1.size() != 1)
+            {
+            q2.push(q1.front());
+             q1.pop();
+            }
+             int s2 = q1.front();
+              q1.pop();
+            
+        while(!q2.empty())
+        {
+            q1.push(q2.front());
+            q2.pop();
+        }
+       
+        return s2;
+}
